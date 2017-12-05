5f519eb893c7ddc2b0e6bccae118db5a59f8c97c
alec <abchapman93@gmail.com>
2017-12-05 15:51:14 -0700

    module for representing texts and annotations

---
+++ annotations/annotations.py
@@ -0,0 +1,11 @@
+"""
+This module defines classes that are used to represent texts and annotations.
+"""
+
+class ClinicalText(object):
+    """
+    This class is a representation of a single clinical documents.
+    It is initialized with `text`, an unprocessed text document.
+    In order to preserve the initial 
+    """
+
